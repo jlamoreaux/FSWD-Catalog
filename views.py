8c7735756c1a817d89403671b8f59dc59eb4a322
Jorcan Lamoreaux <jnacious88@gmail.com>
2017-09-28 14:13:18 -0500

    removed debugging code

--- views.py
+++ views.py
@@ -39,8 +39,6 @@ def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase +
         string.digits) for x in xrange(32))
     login_session['state'] = state
-    print CLIENT_ID
-    print state
     return render_template('login.html', STATE=state, CLIENT_ID=CLIENT_ID)
 
 
